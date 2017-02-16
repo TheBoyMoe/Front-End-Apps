867f82752ce685c01cbc1b2652a2677717c14765
bill fero <billfero@gmail.com>
2017-02-16 19:04:42 +0000

    add input field and button

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
+/*
+	References:
+	
+	examples:
+	[1] http://codepen.io/dishantsoni/pen/pgVeQW *
+	[2] http://codepen.io/mortyyy/pen/woOQKz
+	[3] http://codepen.io/samphree/pen/YNVOxe (show hide complete)
+	[4] http://codepen.io/franklynroth/pen/ZYeaBd *
+	[5] http://codepen.io/szyszak/pen/pNPXVP
+	[6] http://codepen.io/mehra_as/pen/WrMKKr *
+ */
+
 document.addEventListener('DOMContentLoaded', function () {
-	console.log(`Finished loading page`);
+	
 });
